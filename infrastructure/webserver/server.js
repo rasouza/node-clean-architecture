ce6709df5b776de920392def3c7bfe7a9d52a1cc
R. A. Souza <alves.wm@gmail.com>
2021-01-03 22:22:02 -0300

    adds logging capabilities

--- infrastructure/webserver/server.js
+++ infrastructure/webserver/server.js
@@ -1,9 +1,12 @@
 const each = require('lodash/fp/each')
 const restify = require('restify')
+const pino = require('restify-pino-logger')
 
-module.exports = ({ routes }) => {
+module.exports = ({ routes, LoggerConfig }) => {
   const server = restify.createServer()
+
   server.use(restify.plugins.bodyParser())
+  server.use(pino(LoggerConfig))
 
   each(route => {
     server[route.method](route.path, route.handler)
